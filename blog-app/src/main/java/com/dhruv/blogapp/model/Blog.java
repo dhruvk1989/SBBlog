8a96a7bac72ed5410bf16425c8204f8dde45fb6b
dhruvk1989 <dhruvachintya22@gmail.com>
2022-03-17 22:36:58 +0530

    blog entity and blog repo setup

--- blog-app/src/main/java/com/dhruv/blogapp/model/Blog.java
+++ blog-app/src/main/java/com/dhruv/blogapp/model/Blog.java
@@ -1,5 +1,21 @@
 package com.dhruv.blogapp.model;
 
-public class Blog {
+import lombok.*;
 
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "blogs",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+public class Blog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String content;
 }
